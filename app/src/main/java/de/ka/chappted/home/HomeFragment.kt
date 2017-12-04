000fb4e6637454d1059d3d40752866c95622f2ef
Thomas Hofmann <thomas.hofmann@jamitlabs.com>
2017-12-04 14:08:13 +0100

    #3 Add base bottom navigation with back/rotation logic handling

---
+++ app/src/main/java/de/ka/chappted/home/HomeFragment.kt
@@ -0,0 +1,36 @@
+package de.ka.chappted.home
+
+import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import de.ka.chappted.databinding.FragmentMainBinding
+
+/**
+ * The home fragment.
+ */
+class HomeFragment : Fragment() {
+
+    private var binding: FragmentMainBinding? = null
+
+    companion object {
+        fun newInstance(): HomeFragment {
+            return HomeFragment()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        binding = FragmentMainBinding.inflate(inflater)
+
+        context?.let {
+            binding?.viewModel = HomeFragmentViewModel(it)
+        }
+
+        return binding?.root
+    }
+}
---
+++ app/src/main/java/de/ka/chappted/main/MainActivityViewModel.kt
@@ -0,0 +1,18 @@
+package de.ka.chappted.main
+
+import android.content.Context
+import android.support.design.widget.BottomNavigationView
+import android.view.MenuItem
+import android.widget.Toast
+import de.ka.chappted.commons.ViewModel
+
+/**
+ * A view model for showing the main content.
+ *
+ * Created by Thomas Hofmann on 01.12.17.
+ *
+ * @param context the base context
+ */
+class MainActivityViewModel(context: Context) : ViewModel(context) {
+
+}
