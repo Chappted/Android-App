1b7e0839060d35288755dd22acf4afc5d42955f1
Thomas Hofmann <thomas.hofmann@jamitlabs.com>
2017-12-11 14:07:13 +0100

    #5 Add stubs for account management

---
+++ app/src/main/java/de/ka/chappted/auth/AuthActivity.java
@@ -0,0 +1,26 @@
+package de.ka.chappted.auth;
+
+import android.accounts.Account;
+import android.accounts.AccountAuthenticatorActivity;
+import android.os.Bundle;
+
+import de.ka.chappted.R;
+
+/**
+ * A class for authentication procedures.
+ *
+ *
+ * Created by Thomas Hofmann on 08.12.17.
+ */
+public class AuthActivity extends AccountAuthenticatorActivity {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+    }
+}
